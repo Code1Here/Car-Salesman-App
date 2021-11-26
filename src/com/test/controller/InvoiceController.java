2c6afd9e5d2433deea8270cee6628cd1e3648719
Anthony Duran <anthonybduran@gmail.com>
2021-11-26 18:32:41 -0500

    Java App Fin al
    Car salesman pre-dashboard

--- src/com/test/controller/InvoiceController.java
+++ src/com/test/controller/InvoiceController.java
@@ -1,30 +1,90 @@
 package com.test.controller;
 
-        import com.test.view.ViewFactory;
-        import javafx.fxml.FXML;
-        import javafx.fxml.Initializable;
-        import javafx.scene.control.ChoiceBox;
-        import javafx.scene.input.MouseEvent;
+import com.test.view.ViewFactory;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 
-        import java.net.URL;
-        import java.util.ResourceBundle;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class InvoiceController extends BaseController implements Initializable {
+    @FXML
+    private CheckBox CashCheckBox;
 
     @FXML
-    private ChoiceBox<String> creditDebit;
+    private CheckBox CheckCheckBox;
+
+    @FXML
+    private TextField CheckNumberV;
+
+    @FXML
+    private TextField CityStateZipV;
+
+    @FXML
+    private TextField ColorV;
+
+    @FXML
+    private TextField DepositV;
+
+    @FXML
+    private TextField LoanBalanceV;
+
+    @FXML
+    private CheckBox LoanCheckBoxV;
+
+    @FXML
+    private TextField MakeV;
+
+    @FXML
+    private TextField MileageV;
+
+    @FXML
+    private TextField ModelV;
+
+    @FXML
+    private TextField NumberV;
+
+    @FXML
+    private TextField RegistrationV;
+
+    @FXML
+    private CheckBox TotalCheckBoxV;
+
+    @FXML
+    private TextField TotalV;
+
+    @FXML
+    private TextField VINv;
+
+    @FXML
+    private TextField YearV;
+
+    @FXML
+    private TextField buyerNameV;
+
+    @FXML
+    private TextField mailingAddressV;
 
     public InvoiceController(ViewFactory viewFactory, String fxmlName) {
         super(viewFactory, fxmlName);
     }
 
-    @FXML
-    void payment(MouseEvent event) {
-        creditDebit.getSelectionModel().getSelectedItem();
-    }
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
     }
 }
+
+/*
+ import javafx.scene.control.ChoiceBox;
+ import javafx.scene.input.MouseEvent;
+
+ @FXML
+ private ChoiceBox<String> creditDebit;
+
+ @FXML
+ void payment(MouseEvent event) {
+ creditDebit.getSelectionModel().getSelectedItem();
+ }
+ */
